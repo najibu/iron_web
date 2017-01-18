1d83ab7fc9d24994fd41d3f35eba8006f739779d
najibu <ns.najibu@gmail.com>
2017-01-18 18:01:56 +0000

    Added database and master layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+@section('title', 'Home')
+
+@section('content')
+    <div class="jumbotron">
+        <div class="row banner">
+            <div class="col-md-12">
+                <h1 class="text-center margin-top-100 editContent">
+                    Iron Web Test
+                </h1>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+@endsection
